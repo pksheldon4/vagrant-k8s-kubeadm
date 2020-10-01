b16be4b73d97195406e8500084e7f0b8a0df6e73
Preston Sheldon <pksiv@hotmail.com>
2020-10-01 08:57:08 -0400

    Update cluster 1 & 2 to specifically use k8s 1.19.2

--- cluster1/scripts/install-k8s.sh
+++ cluster1/scripts/install-k8s.sh
@@ -4,7 +4,7 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=1.19.2-00 kubeadm=1.19.2-00 kubectl=1.19.2-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 kubeadm config images pull
